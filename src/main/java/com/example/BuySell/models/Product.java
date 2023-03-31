677c3f83839e01f287ef1ebbffecf70efa44a4ab
Pakshaev <pakshaev2003@gmail.com>
2023-03-31 18:47:02 +0400

    Initial commit

---
+++ src/main/java/com/example/BuySell/models/Product.java
@@ -0,0 +1,23 @@
+package com.example.BuySell.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    @Column(columnDefinition = "text")
+    private String description;
+    private int price;
+    private String city;
+    private String author;
+}
